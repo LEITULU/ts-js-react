5611e7b8bf496604bf8a6844744faa296a26a4d9
MSI\231050 <user1>
2025-10-21 15:50:05 +0900

    bug curr

--- src/Mid.tsx
+++ src/Mid.tsx
@@ -2,16 +2,25 @@ import { useState } from "react";
 
 function Mid1() {
     const [todo, setTodo] = useState<string[]>([]);
+    const [inputValue, setInputValue] = useState<string>("");
     
     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setTodo([...todo, e.target.value]);
+        setInputValue(e.target.value);
     };
+
+    const handleClick = () => {
+        if (inputValue.trim() !== "") {
+            setTodo([...todo, inputValue]);
+            setInputValue("");
+        }
+    }
 
     return (
     <>
     <div>
         <h1>Todo list</h1>
-        <input onChange={onChange} />
+        <input value={inputValue} onChange={onChange} />
+        <input type="button" value="button" onClick={handleClick} />
         <ul>
             {todo.map((item, index) => (
                 <li key={index}>{item}</li>
