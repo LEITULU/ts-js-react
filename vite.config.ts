a204949d0ef5cb911f6c0772609e0c6b6b54cbe1
LEITULU <reitsuru51@gmail.com>
2025-10-21 22:28:32 +0900

    git hub pages 3 correction

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/ts-js-react/',
+  build: {
+    outDir: 'docs', // ← distではなくdocsに出力
+  },
   plugins: [react()],
 })
